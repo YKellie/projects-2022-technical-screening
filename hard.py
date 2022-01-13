adbf8c45eab3ad6e41ecfb7794ba744f8bd0e8ca
Kellie Yau <kellie.yau@hotmail.com>
2022-01-13 23:43:24 +1100

    Idea for is_unlocked
    had a go at trying to resolve it using a some brainstorming... Will try some more debugging and conditions tomorrow

--- hard.py
+++ hard.py
@@ -23,6 +23,90 @@ with open("./conditions.json") as f:
     CONDITIONS = json.load(f)
     f.close()
 
+# Bool: Determines if the course had been completed by the student
+def find_course_in_courses_list(courses_list, course):
+    if course in courses_list:
+        return True
+    return False
+
+# Bool: Determines if an "OR" condition is satisfied
+def valid_or_condition(courses_list, words, i):
+    if words[i - 1] in courses_list or words[i + 1] in courses_list:
+        return True
+    
+    # Check if next or if available 
+    # case: ... or ... or ... where first two courses were not found
+    if i + 2 > len(words):
+        return False
+    
+    if words[i + 2] is "or":
+        # recursively calls function
+        return (valid_and_condition(courses_list, words, i + 2))
+    return False
+
+# Bool: determines if "AND" condiiton is satisfied
+def valid_and_condition(courses_list, words, i):
+    if words[i - 1] in courses_list and words[i + 1] in courses_list:
+        return True
+    return False
+
+# Int: Calculates units of credits within completed comp courses based on a given level
+def calc_uoc_in_level(courses_list, level):
+    comp_course_level = "COMP" + level
+    uoc = 0
+    for course in courses_list:
+        if comp_course_level in course:
+            uoc += 6
+    return uoc
+
+# Bool: determines if "units of credit" condition is satisfied
+def valid_units_of_credit(courses_list, words, i):
+    uoc_required = int(words[i - 1])
+    uoc = 0
+
+    # Check extra conditions e.g. "in": COMP, level 1, level 3, (..., ..., ...)
+    if i + 3 < len(words):
+        if (words[i + 3] is "in"):
+            # only comp courses
+            if words[i + 4].lower() is "comp":
+                for course in courses_list:
+                    if "comp" in course.lower():
+                        uoc += 6
+            
+            # level course req            
+            elif i + 5 < len(words):
+                level = words[i + 5].lower() 
+                if level in ["1", "2", "3"]:
+                    uoc = calc_uoc_in_level(courses_list, level)
+
+            # range of courses that meet req  
+            elif i + 4 < len (words): 
+                if "(" in words[i + 4]:
+                    j = i + 4 
+                    while j < len(words):
+                        course = words[j].split("(", ")")
+                        if find_course_in_courses_list(courses_list, course):
+                            uoc += 6
+                        j += 1
+
+    # No extra requirements 
+    else: 
+        uoc = len(courses_list) * 6
+    
+    if (uoc >= uoc_required):
+        return True
+    return False
+
+
+def find_index(words, target):
+    i = 0
+    while i < len(words):
+        if words[i] is target:
+            return i
+        i += 1
+    return None
+
+
 def is_unlocked(courses_list, target_course):
     """Given a list of course codes a student has taken, return true if the target_course 
     can be unlocked by them.
@@ -32,11 +116,51 @@ def is_unlocked(courses_list, target_course):
 
     You can assume all courses are worth 6 units of credit
     """
+    # NOTE: All course codes are unique
     
-    # TODO: COMPLETE THIS FUNCTION!!!
+    subjects_list = ["COMP", "DPST", "MTRN", "ELEC", "MATH"]
+    # Find target course conditions in condition.txt
+    conditions_list = []
+    with open("./conditions.json") as f:
+        CONDITIONS = json.load(f)
+        conditions_list = CONDITIONS.split(":")
+        f.close
+    index = conditions_list.index(target_course)
+    conditions_for_target_course = conditions_list[index + 1]
     
-    return True
+    # Validate if conditions have been met
+    words = conditions_for_target_course.split(" ")
+    # if there are no conditions
+    if not words:
+        return True
+    # There are conditions
+    else:
+        i = 0
+        condition_met = True
+        while i < len(words) and condition_met:
+            # validates "OR" condition
+            word = words[i].lower()
+            if word is "or":
+                if not valid_or_condition:
+                    return False
+            # validate "AND" condition
+            elif word is "and":
+                if not valid_and_condition(courses_list, words, i):
+                    return False
+            # validate "units of credit condition"
+            elif word is "units":
+                if not valid_units_of_credit(courses_list, words, i):
+                    return False
 
+            # Find course codes if they are present in sourses list 
+            else:
+                course_code = word.split(subjects_list)
+                # Find course codes
+                if course_code.isdigit() and int(course_code > 1000):
+                    if not (find_course_in_courses_list(courses_list, course_code)):
+                        return False
+            i += 1
+        return True
 
 
 
